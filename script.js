2010085a14d467604db5baf4d47dc09c8039e947
vnavspd <37489762+vnavspd@users.noreply.github.com>
2026-02-02 10:28:37 -0500

    Update GIFs in index.html, no_page.html, yes_page.html and enhance script.js with new GIF array
    - Replaced GIF URLs in index.html, no_page.html, and yes_page.html for updated visuals.
    - Added a new array of GIF URLs in script.js to dynamically change GIFs based on user interaction.

--- script.js
+++ script.js
@@ -66,6 +66,20 @@ const messages = [
     "Je niaise, dis oui s'il te plaît! ❤️"
 ];
 
+const gifs = [
+    "https://media.giphy.com/media/abZ5exGrse0W4/giphy.gif",
+    "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmxybzhuZzRyeWU4eWR3MGZ5eWc1ZHl5YzlzZjF1bjY0OTN3M3RyZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ffzhLUixCtlsc/giphy.gif",
+    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHY0ZDR0czcxaHhvYXAycm82ajBrejF0cWl0NmNwa3JmZzAwNjNjdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vkxxV45WtCmRTwFTDz/giphy.gif",
+    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeW0yZjdpdnRxdmRmOXd1MXE5MGV6M24yc2loZWVhdmYyNG0xcTZ6MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YZOsKxJfmvzG0/giphy.gif",
+    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzg3cHo0aWYzeXlrYjRpbTd1YzVnbHlnM3ByeGo5NTZyMG5rb2h0YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/rehVUJEdT36DdGTK5A/giphy.gif",
+    "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGY2ZzRndjB1ZHhnd3N2dHMxMnpyZXg2c3Fwamtlc3Bzb2NtajU5NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/dJYoOVAWf2QkU/giphy.gif",
+    "https://media.giphy.com/media/4zUmRD2x9vl06ltMXd/giphy.gif",
+    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeG05NW1oMDRrcTVzNmtoOWJjenRsOTF0ZmQ2MDdwbG03MXFxejc3byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7AzEXdIb1wyCTWJntb/giphy.gif",
+    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGFxdzg4YXB4enFwbXl6ZzR5anRtMHVwMDY4OG04NW96dXB4MmxvYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/awjh48VUptCPbEvYob/giphy.gif",
+    "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXMwZXpvZmU3anoxOXIzaWs1ZDE0dms4MjRmMnlyOGhpZnprbTU0aSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/USFBeBYAY076m5KcKR/giphy.gif",
+    "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcXl0MXN1NHgyd2lsZHMyaXZoamdjaHlqYmt1dzljc2J2N2JkNmExdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/E1sD9HYfQXZRrcMkF1/giphy.gif"
+];
+
 let messageIndex = 0;
 
 function handleYesClick() {
@@ -77,7 +91,10 @@ function handleYesClick() {
     
     const promptText = document.querySelector('.prompt-text');
     const noButton = document.querySelector('.no-button');
+    const gifImage = document.querySelector('.gif_container img');
+    
     promptText.textContent = messages[messageIndex];
+    gifImage.src = gifs[messageIndex];
     messageIndex = messageIndex + 1;
     const currentSize = parseFloat(window.getComputedStyle(noButton).fontSize);
     noButton.style.fontSize = `${currentSize * 1.15}px`;
